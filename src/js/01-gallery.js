80639b1cd41ec9c127dbdd6ee2ef5e4f6593c29c
Natalia Yakovyna <yakovina.n13@gmail.com>
2024-01-28 14:53:53 +0200

    task-02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector('.gallery');
+const galleryItemMurkup = createGalleryItemsMarkup (galleryItems);
+
+
+
+galleryList.insertAdjacentHTML('beforeend', galleryItemMurkup)
+
+// console.log(createGalleryItemsMarkup (galleryItems));
+
+function createGalleryItemsMarkup (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>
+        `;
+    }).join('')
+    
+}
+
+const lightboxGallery = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionPosition:'bottom',
+    captionDelay: 250, });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
 
+
+
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', function getCurrentTime(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+      });
+
+    const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+    // console.log(currentTime);
+
+    player.setCurrentTime(currentTime).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+        
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                console.log(error.name);
+                break;
+            default:
+                console.log('some other error occurred');
+                break;
+        }
+    });
