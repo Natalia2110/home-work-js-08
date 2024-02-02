1fa25275339b201c3a7346dae65acead4967ee47
Natalia Yakovyna <yakovina.n13@gmail.com>
2024-02-02 18:19:25 +0200

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
 
+const  feedbackFormEl= document.querySelector('.feedback-form');
+console.log(feedbackFormEl);
+
+const key = "feedback-form-state";
+
+// feedbackFormEl.elements.value = localStorage.getItem(key) ?? "";
+
+
+
+function setLocalStorage(event) {
+    const feedbackForm = {
+        email: event.target.value,
+        message: event.target.value,
+    }
+    // const evTargetValue = event.target.value;
+    localStorage.setItem(key , JSON.stringify(feedbackForm));
+    
+};
+function removeLocalStorage(event) {
+    event.preventDefault();
+    localStorage.removeItem(localStorageKey);
+    feedbackFormEl.reset();
+  }
+
+
+
+feedbackFormEl.addEventListener('input', setLocalStorage);
+feedbackFormEl.addEventListener("submit", removeLocalStorage);
