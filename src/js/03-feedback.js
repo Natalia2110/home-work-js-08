8ee369676d759113dd84c65b7b77371883acc20f
Natalia Yakovyna <yakovina.n13@gmail.com>
2024-03-18 14:47:59 +0200

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,41 @@
 import throttle from 'lodash.throttle';
 
-
-const  feedbackFormEl= document.querySelector('.feedback-form');
-console.log(feedbackFormEl);
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textArea: document.querySelector('.feedback-form textarea'),
+};
+// const  feedbackFormEl= document.querySelector('.feedback-form');
+console.log(refs.form);
+console.log(refs.input);
+console.log(refs.textArea);
 
 const localStorageKey = "feedback-form-state";
 
-feedbackFormEl.addEventListener('input', throttle(onFormInput, 500));
-feedbackFormEl.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
 
 // feedbackFormEl.elements.value = localStorage.getItem(key) ?? "";
 
 let dataForm = JSON.parse(localStorage.getItem(localStorageKey)) || {};
-const { email, message } = form.elements;
+const { input, textArea } = refs;
+// const { email, message } = refs.form.elements;
 reloadPage();
+
+function reloadPage() {
+  if (dataForm) {
+    input.value = dataForm.input || '';
+    textArea.value = dataForm.textArea || '';
+  }
+}
+
+
+
+// console.log(input);
 
 function onFormInput(event) {
 
-  dataForm = { email: email.value, message: message.value };
+  dataForm = { input: input.value, textArea: textArea.value };
   localStorage.setItem(localStorageKey, JSON.stringify(dataForm));
     
     // const feedbackForm = {
@@ -30,25 +48,21 @@ function onFormInput(event) {
     
 };
 
-function reloadPage() {
-  if (dataForm) {
-    email.value = dataForm.email || '';
-    message.value = dataForm.message || '';
-  }
-}
+
 
 function onFormSubmit(event) {
     event.preventDefault();
     // localStorage.removeItem(localStorageKey);
-    console.log({ email: email.value, message: message.value });
+    console.log({ email: input.value, message: textArea.value });
 
-    if (email.value === '' || message.value === '') {
+    if (input.value === '' || textArea.value === '') {
       return alert('Please fill in all the fields!');
     }
 
-    localStorage.removeItem(localStorageKey);
+    // localStorage.removeItem(localStorageKey);
     event.currentTarget.reset();
     dataForm = {};
   };
 
+  // console.log(dataForm);
 
